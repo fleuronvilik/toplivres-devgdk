4f62bec98b75d5e7054479a35745eac9e6a6fe99
Fleuron Gnadekpa <vilfleur@gmail.com>
2025-08-13 12:03:06 +0200

    update book model and add migrations folder

--- app/models.py
+++ app/models.py
@@ -2,4 +2,5 @@ from app import db
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable = False)
+    title = db.Column(db.String(120), nullable=False)
+    unit_price = db.Column(db.Float, nullable=False)
