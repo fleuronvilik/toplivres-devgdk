d54e8011bfac06d2e1a2f0a2527fd03503d5b80e
Fleuron Gnadekpa <vilfleur@gmail.com>
2025-08-14 18:42:30 +0200

    feat(models): from customer to user

--- app/models.py
+++ app/models.py
@@ -10,22 +10,39 @@ class Series(BaseModel):
     __tablename__ = 'book_series'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text, unique=True, nullable=False)
     books = db.relationship('Book', backref='series', cascade='all, delete-orphan')
 
     __table_args__ = (
         db.UniqueConstraint('name', name='uq_book_series_name'),
     )
 
-
 class Book(BaseModel):
     __tablename__ = 'book' # not neccessary
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    serie_id = db.Column(
+    series_id = db.Column(
         db.Integer,
         db.ForeignKey('book_series.id', name='fk_book_series_id'),
         nullable=True
     )
     unit_price = db.Column(db.Float, nullable=False)
+
+class User(BaseModel):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.String(10), default="customer", nullable=False)
+
+    store_name = db.Column(db.String(30))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(20))
+
+    __table_args__ = (
+        db.UniqueConstraint('name', name='uq_customer_name'),
+        db.UniqueConstraint('email', name='uq_customer_email')
+    )
