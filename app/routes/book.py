f32fa9a2dff08500cbbd9669b4d36bc0a538b16e
Fleuron Gnadekpa <vilfleur@gmail.com>
2025-08-15 08:30:24 +0200

    feat(routes): add book for admin only

--- app/routes/book.py
+++ app/routes/book.py
@@ -3,12 +3,20 @@ from app import db
 from app.models import Book
 from app.schemas import BookSchema
 
+from flask_jwt_extended import jwt_required, get_jwt
+
 book_schema, books_schema = BookSchema(), BookSchema(many=True)
 
 main = Blueprint('main', __name__, url_prefix='/books')
 
 @main.route('/', methods=["POST"])
+@jwt_required()
 def add_book():
+    claims = get_jwt()
+
+    if claims.get("role") == "customer":
+        return jsonify({"message": "admin only"}), 403
+
     data = request.get_json()
     errors = book_schema.validate(data, session=db.session)
     if errors:
